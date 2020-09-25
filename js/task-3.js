a6b9cf9eaa6d9e2677eb67fcd98a91a8b5518334
Katarina Kyrylenko <katarina.kyrylenko@gmail.com>
2020-09-25 23:20:33 +0300

    create task-3

---
+++ js/task-3.js
@@ -0,0 +1,17 @@
+const userPassword = "jqueryismyjam";
+
+const ADMIN_PASSWORD = "jqueryismyjam";
+
+const CANCELED_BY_USER = "Отменено пользователем!";
+const ACCESS_IS_ALLOWED = "Добро пожаловать!";
+const ACCESS_DENIED = "Доступ запрещен, неверный пароль!";
+let message;
+
+// Write code under this line
+if (userPassword === ADMIN_PASSWORD) {
+  message = "Добро пожаловать!";
+} else if (userPassword === null) {
+  message = "Отменено пользователем!";
+} else {
+  message = "Доступ запрещен, неверный пароль!";
+}
